2dbdecf4a3365549528dfdc9cf800059d6fd251e
MarselHapa5 <93425197+MarselHapa5@users.noreply.github.com>
2025-05-15 15:56:26 +0400

    Started import script from py to C

---
+++ nickGen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Код не закончен!!! Code doesn't ended!!!
+
+int main(){
+    const char consonants[] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+    const char vowels[] = {'a','e','i','o','u','y'};
+
+    char * nick = "";
+}
